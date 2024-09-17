66251dcc82901800b41592e3bdbbfffccf91114e
Maxine <maxinework392@gmail.com>
2024-09-17 13:14:50 +0100

    fixed code error

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        system.out.printin(sayHelloToSomeone("Maxine tries again!"))
+        system.out.printin(sayHelloToSomeone("Maxine tries again!"));
         System.out.println(sayGoodbye());
     }
 
