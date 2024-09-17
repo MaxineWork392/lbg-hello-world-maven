df3f07c97c575cd459d2e6c0434bd31c8dd607e5
Maxine <maxinework392@gmail.com>
2024-09-17 13:01:25 +0100

    Add new hello world

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        system.out.printin(sayHelloToSomeone("Maxine"))
         System.out.println(sayGoodbye());
     }
 
