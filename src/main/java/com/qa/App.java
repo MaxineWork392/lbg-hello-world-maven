a14dc3ffa72f47acf7f2a1a9e15dd85dbfd8b35c
Maxine <maxinework392@gmail.com>
2024-09-17 13:09:31 +0100

    new update

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        system.out.printin(sayHelloToSomeone("Maxine"))
+        system.out.printin(sayHelloToSomeone("Maxine2"))
         System.out.println(sayGoodbye());
     }
 
